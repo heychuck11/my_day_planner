7d5b927eb44168de42c76fc4736de47f2120a520
Charlie Howard <chuckhoward99@gmail.com>
2020-10-03 23:33:50 -0600

    updated homework

--- script.js
+++ script.js
@@ -31,18 +31,18 @@ $(document).ready(function(){
   hourUpdater();
   // var interval as set interval run hour updater every 15 seconds
 
-  var interval =
+  var interval = setInterval(hourUpdater, 15000 )
 
 
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-9 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-9 .description").val(localStorage.getItem("hour=11"));
-  $("#hour-9 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-9 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-9 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-9 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-9 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-9 .description").val(localStorage.getItem("hour-17"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour=11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
